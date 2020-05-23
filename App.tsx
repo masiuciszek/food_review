023bcd9a28993991e556c7c991d750a47d2a3041
Masiu <ciszekmarcell@gmail.com>
2020-05-23 13:34:57 +0200

    simple server added

--- App.tsx
+++ App.tsx
@@ -9,44 +9,22 @@ import {
 } from './src/components/styles/Common'
 import RestaurantRow from './src/components/ResturantRow'
 
-const restaurants: RestaurantType[] = [
-  {name: 'React Cafe', address: '123 Anywhere St'},
-  {name: 'Fancy Restaurant', address: '799 Main St'},
-  {name: 'Taco Place', address: '550 Maple Rd'},
-  {name: "Tony's Diner", address: '4101 College St'},
-  {name: 'Pasta Central', address: '706 Harper St'},
-  {name: 'Burger Builder', address: '4869 Hamilton Dr'},
-  {name: 'Pizza Express', address: '1049 Bird St'},
-  {name: 'Teriyaki To Go', address: '1885 Tea Berry Lane'},
-  {name: 'Maroon Deli', address: '1082 Stuart St'},
-  {name: 'Prime Bar and Grill', address: '1848 Fairfax Dr'},
-  {name: 'Dumpling House', address: '747 Kelly Dr'},
-  {name: 'Hot Chicken', address: '1816 Olive St'},
-  {name: "Luna's Tap Room", address: '3256 Spirit Dr'},
-  {name: 'Quick Sandwich Shop', address: '2587 Cherry Ridge Dr'},
-  {name: "Bobby's Burgers", address: '4152 Berkley St'},
-  {name: 'Turnpike Diner', address: '4571 Central Ave'},
-  {name: 'Bombay Express', address: '65 Queens Lane'},
-  {name: 'Coffee Central', address: '3228 Oakwood Circle'},
-  {name: "King's Garden", address: '2935 Victoria Ct'},
-  {name: 'Salads and More', address: '2454 Preston St'},
-]
-
 const App: React.FC = () => {
-  const [data, setData] = useState<Array<RestaurantType>>(restaurants)
+  const [restaurants, setRestaurants] = useState<Array<RestaurantType>>([])
   const [search, setSearch] = useState<string>('')
-  const [testData, setTestData] = useState([])
 
   React.useEffect(() => {
-    if (search.length === 0) {
-      setSearch('')
-      setData(restaurants)
+    const getRestaurants = async () => {
+      const res = await fetch('http://localhost:3000/restaurants')
+      const data = await res.json()
+      setRestaurants(data)
     }
-  }, [data])
+    getRestaurants()
+  }, [restaurants])
 
   const handleSearch = (text: string) => {
     setSearch(text)
-    setData((prev) =>
+    setRestaurants((prev) =>
       prev.filter(
         (s) =>
           s.address.toLowerCase().includes(text.toLowerCase()) ||
@@ -67,7 +45,10 @@ const App: React.FC = () => {
       <SearchInput placeholder="..search" onChangeText={handleSearch} />
 
       <FlatList
-        data={data}
+        data={restaurants.filter(
+          (r) =>
+            !search || r.name.toLowerCase().indexOf(search.toLowerCase()) > -1,
+        )}
         renderItem={({item, index}) => (
           <RestaurantRow item={item} index={index} />
         )}
